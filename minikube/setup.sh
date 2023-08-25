4c5e0887bf959f7117e69695db4330676bcf0e92
CodingManoj <mvp2612@hotmail.com>
2023-08-25 19:57:12 -0400

    Git Auto Push

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -7,6 +7,10 @@ if [ ! -f "~/.ssh/id_rsa.pub" ]; then
   cat /dev/zero | ssh-keygen -q -N ""
 fi
 
+echo "Ensure you open the following URL and subscribe. Wait for Subscription to Complete and Press Enter to Continue"
+echo "https://aws.amazon.com/marketplace/pp/prodview-foff247vr2zfw?ref_=aws-mp-console-subscription-detail"
+read -p ""
+
 mkdir -p ~/.kube
 curl -s https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/terraform/install.sh | sudo bash
 curl -s https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/terraform/k8-stack/install.sh | sudo bash
