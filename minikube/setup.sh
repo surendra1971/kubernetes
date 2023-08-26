25ddfdb1f503cd167ee11a8f10aa5002b10ea6ac
CodingManoj <mvp2612@hotmail.com>
2023-08-25 20:13:11 -0400

    Git Auto Push

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -3,9 +3,9 @@
 read -p 'Enter DNS Hosted Zone Name: ' hname
 export TF_VAR_HOSTED_ZONE=${hname}
 
-if [ ! -f "~/.ssh/id_rsa.pub" ]; then
-  cat /dev/zero | ssh-keygen -q -N ""
-fi
+# if [ ! -f "~/.ssh/id_rsa.pub" ]; then
+#   cat /dev/zero | ssh-keygen -q -N ""
+# fi
 
 echo "Ensure you open the following URL and subscribe. Wait for Subscription to Complete and Press Enter to Continue"
 echo "https://aws.amazon.com/marketplace/pp/prodview-foff247vr2zfw?ref_=aws-mp-console-subscription-detail"
